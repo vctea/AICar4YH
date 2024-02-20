453eb5b391a02c5b1ee64145ee3fcd78a8bc5925
vctea <59410381@qq.com>
2024-02-20 21:51:33 +0800

    modify run funtion.

--- aicar4yh.py
+++ aicar4yh.py
@@ -6,4 +6,4 @@ from app import create_app, socketio
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=80)
